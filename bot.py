60c364d904888d8e4b1527c668052e031025717e
colussiav <colussi.av@gmail.com>
2026-07-14 11:50:42 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ SYSTEM_INSTRUCTION = (
 )
 
 def consultar_gemini(prompt_usuario):
-    url = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={GEMINI_API_KEY}"
     headers = {"Content-Type": "application/json"}
     
     # Pasamos la instrucción del sistema como un rol de sistema antes del mensaje del usuario
